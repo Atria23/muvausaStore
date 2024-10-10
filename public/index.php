178f9b5b668e561d42522263ea9eea682d133cdc
Atria23 <danu03anggoro@gmail.com>
2024-10-10 22:26:21 +0700

    change index

--- public/index.php
+++ public/index.php
@@ -1,3 +1,5 @@
+
+text/x-generic index.php ( PHP script, ASCII text )
 <?php
 
 use Illuminate\Http\Request;
@@ -15,3 +17,19 @@ require __DIR__.'/../muvausastore/vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../muvausastore/bootstrap/app.php')
     ->handleRequest(Request::capture());
+
+// use Illuminate\Http\Request;
+
+// define('LARAVEL_START', microtime(true));
+
+// // Determine if the application is in maintenance mode...
+// if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+//     require $maintenance;
+// }
+
+// // Register the Composer autoloader...
+// require __DIR__.'/../vendor/autoload.php';
+
+// // Bootstrap Laravel and handle the request...
+// (require_once __DIR__.'/../bootstrap/app.php')
+//     ->handleRequest(Request::capture());
